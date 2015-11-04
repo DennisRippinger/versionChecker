4cdf6e41a07826e4e31bb60b6934a575b835a59f
Dennis Rippinger <dennis.rippinger@gmail.com>
2015-11-05 00:00:44 +0100

    Initial Commit

---
+++ assets/js/wmCompare.js
@@ -0,0 +1,49 @@
+function compare() {
+    var inputCurrent = document.getElementById("input-current");
+    var inputAvailable = document.getElementById("input-available");
+    var inputCurrentArray = inputCurrent.value.split("\n");
+    var inputAvailableArray = inputAvailable.value.split("\n");
+    var counter = 0;
+    for (var _i = 0; _i < inputCurrentArray.length; _i++) {
+        var current = inputCurrentArray[_i];
+        var referenceNumber = extractNumber(current);
+        var finding = "";
+        var highestNumber = 0;
+        for (var _o = 0; _o < inputAvailableArray.length; _o++) {
+            var available = inputAvailableArray[_o];
+            if (current.substr(0, lastUnderscorePosition(current)) == available.substr(0, lastUnderscorePosition(available))) {
+                var availableVersion = extractNumber(available);
+                if (referenceNumber < availableVersion && availableVersion > highestNumber) {
+                    finding = available;
+                    highestNumber = availableVersion;
+                }
+            }
+        }
+        if (finding.length > 0) {
+            var resultTable = document.getElementById("resultTableBody");
+            var row = resultTable.insertRow(counter++);
+            var numberCell = row.insertCell(0);
+            numberCell.innerHTML = counter.toLocaleString();
+            var findingsCell = row.insertCell(1);
+            findingsCell.innerHTML = current;
+            var currentCell = row.insertCell(2);
+            currentCell.innerHTML = finding;
+        }
+    }
+}
+function extractNumber(inputString) {
+    var matches = inputString.match(/\d+$/);
+    if (matches) {
+        var number = Number(matches[0]);
+        return number;
+    }
+    return -1;
+}
+function lastUnderscorePosition(inputString) {
+    for (var _i = inputString.length; _i >= 0; _i--) {
+        var char = inputString[_i];
+        if (char == "_") {
+            return _i;
+        }
+    }
+}
---
+++ assets/js/wmCompare.ts
@@ -0,0 +1,55 @@
+function compare() {
+  var inputCurrent = <HTMLInputElement> document.getElementById("input-current");
+  var inputAvailable = <HTMLInputElement> document.getElementById("input-available");
+
+  var inputCurrentArray = inputCurrent.value.split("\n");
+  var inputAvailableArray = inputAvailable.value.split("\n");
+  var counter = 0;
+
+  for (var _i = 0; _i < inputCurrentArray.length; _i++) {
+    var current = inputCurrentArray[_i];
+    var referenceNumber = extractNumber(current);
+    var finding = "";
+    var highestNumber = 0;
+    for (var _o = 0; _o < inputAvailableArray.length; _o++) {
+      var available = inputAvailableArray[_o];
+
+      if (current.substr(0, lastUnderscorePosition(current)) == available.substr(0, lastUnderscorePosition(available))) {
+        var availableVersion = extractNumber(available);
+        if (referenceNumber < availableVersion && availableVersion > highestNumber) {
+          finding = available;
+          highestNumber = availableVersion;
+        }
+      }
+    }
+
+    if (finding.length > 0) {
+      var resultTable = <HTMLTableElement> document.getElementById("resultTableBody");
+      var row = <HTMLTableRowElement> resultTable.insertRow(counter++);
+      var numberCell = row.insertCell(0);
+      numberCell.innerHTML = counter.toLocaleString();
+      var findingsCell = row.insertCell(1);
+      findingsCell.innerHTML = current;
+      var currentCell = row.insertCell(2);
+      currentCell.innerHTML = finding;
+    }
+  }
+}
+
+function extractNumber(inputString: string) {
+  var matches = inputString.match(/\d+$/);
+  if (matches) {
+    var number = Number(matches[0]);
+    return number;
+  }
+  return -1;
+}
+
+function lastUnderscorePosition(inputString: string) {
+  for (var _i = inputString.length; _i >= 0; _i--) {
+    var char = inputString[_i];
+    if (char == "_") {
+      return _i;
+    }
+  }
+}
